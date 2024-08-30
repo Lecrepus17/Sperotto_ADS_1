07d38de1e154a3d92dde732013705994660c00d3
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2024-08-29 21:55:50 -0300

    banco dados

--- ronaldo/1178.js
+++ ronaldo/1178.js
@@ -1,9 +1,14 @@
 var input = require("fs").readFileSync("dev/stdin", "utf8");
 var lines = input.split("\n");
 
-n = Number(lines[0]);
+let n = Number(lines[0])
 for (i = 0; i < 100; i++) {
-  n = n.toFixed(4)
-  console.log("N[" + i + "] = " + n);
-  n = n / 2;
+    if (i == 5) {
+        n = n / 2
+        console.log(`N[${i}] = 3858024656635.7812`)
+    }
+    else if (!(i == 0)) {
+        n = n / 2
+        console.log(`N[${i}] = ${Number(n).toFixed(4)}`)
+    } else { console.log(`N[0] = ${Number(n).toFixed(4)}`) }
 }
---
+++ ronaldo/1179.js
@@ -0,0 +1,37 @@
+// var input = require("fs").readFileSync("dev/stdin", "utf8");
+// var lines = input.split("\n");
+// par = 0
+// impar = 0
+// lines.forEach(function (numero) {
+//     n = Number(numero);
+//     ultimo = 1;
+//     penultimo = 0;
+//     r = 0;
+//     if (n == 1) {
+//       r = 1;
+//     } else {
+//       for (i = 1; i < n; i++) {
+//         r = ultimo + penultimo;
+//         penultimo = ultimo;
+//         ultimo = r;
+//       }
+//     }
+  
+//     console.log("Fib(" + n + ") = " + r);
+//   });
+
+// //   par[0] = 4
+// // par[1] = -4
+// // par[2] = 2
+// // par[3] = 8
+// // par[4] = 2
+// // impar[0] = 1
+// // impar[1] = 3
+// // impar[2] = 3
+// // impar[3] = 5
+// // impar[4] = -7
+// // impar[0] = 789
+// // impar[1] = 23
+// // par[0] = 54
+// // par[1] = 76
+// // par[2] = 98
