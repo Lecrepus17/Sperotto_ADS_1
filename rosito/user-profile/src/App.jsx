416a4976174c4441c9af435c1faf3ccb2b080376
Pedro Sperotto <pedro.eduardo1777@gmail.com>
2024-09-30 21:28:39 -0300

    Uso de React.memo

---
+++ rosito/user-profile/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <>
+      <h1>Exemplo de React.memo com Várias Props</h1>
+      <UserProfile />
+    </>
+  );
+}
+
+export default App;
