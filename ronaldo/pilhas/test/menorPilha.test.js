1c1e09e6ba4687f17ac3ce419d67c5675b870bd7
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2024-10-11 19:44:58 -0300

    Menor pilha

---
+++ ronaldo/pilhas/test/menorPilha.test.js
@@ -0,0 +1,40 @@
+const { Stack } = require("../menorPilha");
+
+describe("Menor da Pilha", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  test("Deve retornar 3 após a sequência de operações PUSH e MIN", () => {
+    stack.push(5);
+    stack.push(7);
+    stack.push(3);
+    stack.push(8);
+    stack.push(10);
+    expect(stack.getMin()).toBe(3);
+
+    stack.pop();
+    stack.pop();
+    expect(stack.getMin()).toBe(3);
+
+    stack.pop();
+    expect(stack.getMin()).toBe(5);
+  });
+
+  test("Deve retornar 50 após a sequência de operações PUSH, POP e MIN", () => {
+    stack.push(100);
+    stack.push(50);
+    expect(stack.getMin()).toBe(50);
+
+    stack.push(45);
+    expect(stack.getMin()).toBe(45);
+
+    stack.pop();
+    expect(stack.getMin()).toBe(50);
+
+    stack.pop();
+    expect(stack.getMin()).toBe(100);
+  });
+});
