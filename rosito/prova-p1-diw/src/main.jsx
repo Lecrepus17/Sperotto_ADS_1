ee22040fbe192015cbb820413254332f33feff94
Pedro Sperotto <pedro.eduardo1777@gmail.com>
2024-10-14 19:54:05 -0300

    ajute passado para main.jsx

--- rosito/prova-p1-diw/src/main.jsx
+++ rosito/prova-p1-diw/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import SchoolHomePage from "./components/SchoolHomePage";
+import Classroom from "./components/Classroom";
+import StudentList from "./components/StudentList";
+import Teacher from "./components/Teacher";
+const turma = {
+  nomeDaTurma: "Turma A",
+  estudantes: [
+    { nome: "João", nota: 8 },
+    { nome: "Maria", nota: 9 },
+    { nome: "Carlos", nota: 7 },
+  ],
+};
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <SchoolHomePage />
+    <Classroom nomeDaTurma={turma.nomeDaTurma} estudantes={turma.estudantes} />
+    <StudentList />
+    <Teacher />
+  </StrictMode>
+);
