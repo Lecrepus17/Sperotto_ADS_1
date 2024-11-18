a31104f4d05e1f4163d5a1388982950a46fa9278
Pedro Sperotto <pedro.eduardo1777@gmail.com>
2024-11-18 20:44:08 -0300

    Atifidate adição de api crud

---
+++ rosito/my-api-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MyAll from './components/MyAll';
+
+function App() {
+  return (
+    <>
+      {/* <MyGet> </MyGet> */}
+      {/* <MyPost> </MyPost> */}
+      <MyAll> </MyAll>
+    </>
+  )
+}
+
+export default App
