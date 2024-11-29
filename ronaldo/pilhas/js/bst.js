73e1a4d18b4d1905014a710d1aa0d5d4cdcd195b
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2024-11-29 20:28:10 -0300

    Início de arvore

---
+++ ronaldo/pilhas/js/bst.js
@@ -0,0 +1,13 @@
+class BinarySearchTree {
+    constructor(){
+        this._root = null;
+    }
+}
+
+class Node {
+  constructor(key) {
+    this._key = key;
+    this._left = null;
+    this._right = null;
+  }
+}
