acd396dc3f02c366db49c33f834971dfef07c857
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2025-02-25 21:20:26 -0300

    alterado nome pasta, inicio node backend

---
+++ desenvolvimento_servicos_web/servidor-celsius-fahrenheit/server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const url = require("url");
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === "/temp") {
+    const celcius = parseFloat(parsedUrl.query.grausCelcius);
+    if (isNaN(celcius)) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ erro: "Parâmetros inválidos" }));
+      return;
+    }
+    const fahrenheit = (celcius * 9 / 5) + 32;
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ fahrenheit }));
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Rota não encontrada");
+  }
+});
+server.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
+
+// que consiga converter uma
+// temperatura em graus Celsius para Fahrenheit, pode usar a fórmula fahrenheit = (celsius * 9/5) +
+// 32;
